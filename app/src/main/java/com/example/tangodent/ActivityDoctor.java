5587e5e8b6a843d9b4bae28bdfd603c8d2ad3ae2
jsschuller <sebastian.schuler@campusfp.es>
2019-04-11 10:39:41 +0200

    Inicio del diseño de la sesion del Doctor

--- app/src/main/java/com/example/tangodent/ActivityDoctor.java
+++ app/src/main/java/com/example/tangodent/ActivityDoctor.java
@@ -2,12 +2,35 @@ package com.example.tangodent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class ActivityDoctor extends AppCompatActivity {
+public class ActivityDoctor extends AppCompatActivity implements View.OnClickListener{
+
+    Button botonCita,botonEventos,botonOpciones;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_doctor);
+
+        botonCita=findViewById(R.id.botonTomaCitas);
+        botonEventos=findViewById(R.id.botonCalendario);
+        botonOpciones=findViewById(R.id.botonOpcionesUser);
+
+        botonCita.setOnClickListener(this);
+        botonEventos.setOnClickListener(this);
+        botonOpciones.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch(v.getId()){
+
+
+
+
+
+        }
     }
 }
