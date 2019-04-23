af25e9b1bebb739ac22a43a231d83694a56ee62f
jsschuller <sebastian.schuler@campusfp.es>
2019-04-23 13:37:25 +0200

    Establezco la repeticion de la contraseña, controlo el problema de los campos vacíos y diseño de las citas

--- app/src/main/java/com/example/tangodent/FuncionesDoctor/ActivityCitas.java
+++ app/src/main/java/com/example/tangodent/FuncionesDoctor/ActivityCitas.java
@@ -1,9 +1,18 @@
 package com.example.tangodent.FuncionesDoctor;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import com.example.tangodent.R;
+import android.util.Log;
 
+import com.example.tangodent.Objetos.FirebaseReferences;
+import com.example.tangodent.Objetos.Usuario;
+import com.example.tangodent.R;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
 
 public class ActivityCitas extends AppCompatActivity {
@@ -12,6 +21,24 @@ public class ActivityCitas extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_citas);
+
+
+        FirebaseDatabase database= FirebaseDatabase.getInstance();
+        DatabaseReference myRef= database.getReference(FirebaseReferences.USUARIO_REFERENCE);
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                String datos= dataSnapshot.getValue(String.class);
+
+                Log.i("DATOS", datos);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
 
 
 
--- app/src/main/java/com/example/tangodent/Main2Activity.java
+++ app/src/main/java/com/example/tangodent/Main2Activity.java
@@ -1,12 +1,15 @@
 package com.example.tangodent;
 
+import android.content.DialogInterface;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import com.example.tangodent.Objetos.FirebaseReferences;
 import com.example.tangodent.Objetos.Usuario;
@@ -35,45 +38,114 @@ public class Main2Activity extends AppCompatActivity implements View.OnClickList
         pass2 = findViewById(R.id.textPass2);
 
         guardar = findViewById(R.id.botonGuardar);
-
         guardar.setOnClickListener(this);
 
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("¡Perfecto!")
+                .setMessage("Se ha creado el usuario correctamente")
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                System.out.println("Hola buenas tardes");
+                            }
+                        });
+        builder.create();
 
     }
 
 
     @Override
     public void onClick(View v) {
-        String nombre_Registro= nombre.getText().toString();
-        String apellido_Registro= apellido.getText().toString();
-        String direccion_Registro= direccion.getText().toString();
-        String fechaNacimiento_Registro= fechaNacimiento.getText().toString();
+        String nombre_Registro = nombre.getText().toString();
+        String apellido_Registro = apellido.getText().toString();
+        String direccion_Registro = direccion.getText().toString();
+        String fechaNacimiento_Registro = fechaNacimiento.getText().toString();
         String email_Registro = email.getText().toString();
         String pass_Registro = pass.getText().toString();
-
-        registrar(email_Registro, pass_Registro);
-
+        String pass2_Registro=pass2.getText().toString();
         FirebaseDatabase database = FirebaseDatabase.getInstance();
         DatabaseReference usuariosRef = database.getReference(FirebaseReferences.FIREBASE_REFERENCE);
+        if (pass_Registro.equals(pass2_Registro)) {
+            if (email_Registro.equals("") || pass_Registro.equals("") || nombre_Registro.equals("") || apellido_Registro.equals("") || direccion_Registro.equals("") || fechaNacimiento_Registro.equals("")) {
+                DialogErrorCampos();
+            } else {
+                registrar(email_Registro, pass_Registro);
+                Usuario usuario = new Usuario(nombre_Registro, apellido_Registro, direccion_Registro, fechaNacimiento_Registro, email_Registro, pass_Registro);
+                usuariosRef.child(FirebaseReferences.USUARIO_REFERENCE).push().setValue(usuario);
 
+            }
 
-        Usuario usuario = new Usuario(nombre_Registro,apellido_Registro,direccion_Registro,fechaNacimiento_Registro,email_Registro,pass_Registro);
-        usuariosRef.child(FirebaseReferences.USUARIO_REFERENCE).push().setValue(usuario);
-
+        }
     }
 
 
     private void registrar(String email, String pass) {
-        FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
-            @Override
-            public void onComplete(@NonNull Task<AuthResult> task) {
-                if (task.isSuccessful()) {
-                    Log.i("SESION", "Usuario creado correctamente");
-                } else {
-                    Log.e("SESION", task.getException().getMessage() + "");
+        if (email.equals("") || pass.equals("")) {
+            DialogErrorCampos();
+        } else {
+            FirebaseAuth.getInstance().createUserWithEmailAndPassword(email, pass).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                @Override
+                public void onComplete(@NonNull Task<AuthResult> task) {
+                    if (task.isSuccessful()) {
+                        DialogRegistro();
+                        Log.i("SESION", "Usuario creado correctamente");
+                    } else {
+                        DialogErrorRegistro();
+                        Log.e("SESION", task.getException().getMessage() + "");
+                    }
                 }
-            }
-        });
+            });
+
+        }
+    }
+
+    public AlertDialog DialogErrorCampos() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+
+        builder.setTitle("Error")
+                .setMessage("Rellena todos los campos para registrarte")
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+        return builder.create();
+    }
+
+    public AlertDialog DialogErrorRegistro() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getApplicationContext());
+
+        builder.setTitle("Error")
+                .setMessage("Error al crear el usuario")
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+
+                            }
+                        });
+
+        return builder.create();
+    }
+    public AlertDialog DialogRegistro() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+
+        builder.setTitle("¡Perfecto!")
+                .setMessage("Se ha creado el usuario correctamente")
+                .setPositiveButton("OK",
+                        new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                System.out.println("Hola buenas tardes");
+                            }
+                        });
+
+        return builder.create();
     }
 
 }
