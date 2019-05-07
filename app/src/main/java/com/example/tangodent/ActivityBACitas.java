4a308122a8d95cfe5851ae9fe72b485968a31ad4
jsschuller <sebastian.schuler@campusfp.es>
2019-05-07 16:40:53 +0200

    Pequeño inicio al BA de citas

--- app/src/main/java/com/example/tangodent/ActivityBACitas.java
+++ app/src/main/java/com/example/tangodent/ActivityBACitas.java
@@ -7,11 +7,22 @@ import android.widget.EditText;
 
 public class ActivityBACitas extends AppCompatActivity {
 EditText ed_Servicio,ed_Nombre,ed_DNI,ed_Email,ed_Telefono,ed_Direccion,ed_Ciudad;
-Button boton_Actualizar,boton_;
+Button boton_Actualizar,boton_Buscar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bacitas);
+
+        ed_Servicio= findViewById(R.id.text_Servicio);
+        ed_Nombre= findViewById(R.id.text_);
+        ed_DNI= findViewById(R.id.text_Servicio);
+        ed_Email= findViewById(R.id.text_Servicio);
+        ed_Telefono= findViewById(R.id.text_Servicio);
+        ed_Direccion= findViewById(R.id.text_Servicio);
+        ed_Ciudad= findViewById(R.id.text_Servicio);
+
+
+
 
 
 
