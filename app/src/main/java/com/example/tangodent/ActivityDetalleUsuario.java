7ead3eac31ae02f078761a294e5418e650026f82
jsschuller <sebastian.schuler@campusfp.es>
2019-05-14 15:35:51 +0200

    Creo Activity para el detalle de usuario

---
+++ app/src/main/java/com/example/tangodent/ActivityDetalleUsuario.java
@@ -0,0 +1,15 @@
+package com.example.tangodent;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ActivityDetalleUsuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_detalle_usuario);
+
+
+    }
+}
